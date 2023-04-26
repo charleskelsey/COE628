8f4d2578c00dbb755f11604133e04194484a710b
charleskelsey <130581038+charleskelsey@users.noreply.github.com>
2023-04-26 15:53:04 -0400

    Add files via upload

---
+++ lab5/main.c
@@ -0,0 +1,45 @@
+#include  <stdio.h> /* printf */
+#include  <sys/types.h> /* pid_t */
+#include <unistd.h> /* get_pid */
+#include <stdlib.h>     /* exit, EXIT_FAILURE */
+#include <sys/wait.h>  /* wait */
+#include <pthread.h>
+#include <sys/stat.h>
+
+void * count(void *);
+int globalNumber = 0;
+pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER;
+//Create a mutex
+
+int main(void) {
+    int i;
+    pthread_t counterThread[5];
+
+    for (int i=0; i<5; i++) {
+        pthread_create(&counterThread[i],NULL,count,NULL);
+    }
+    for (int i=0; i<5; i++) {
+        pthread_join(counterThread[i],NULL);
+    }
+    //Create the 5 threads
+    //Wait for all treads to finish
+
+    return (0);
+}
+
+void * count(void * junk) {
+    int loopCount = 0;
+    pthread_mutex_lock(&mutex1);
+    while (loopCount < 10) {
+        int tmpNumber = globalNumber;
+        printf("counter: %d, Thread: %ld, PID: %d\n",
+                tmpNumber, pthread_self(), getpid());
+        tmpNumber = globalNumber;
+        tmpNumber++;
+        usleep(random() % 2);
+        globalNumber = tmpNumber;
+        loopCount++;
+    }
+    pthread_mutex_unlock(&mutex1);
+    return (0);
+}
