fb3596a4b8c02e6e197831d49ff0a93e6c3d3421
charleskelsey <130581038+charleskelsey@users.noreply.github.com>
2023-04-26 15:41:10 -0400

    Add files via upload

---
+++ lab2/lab2b/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define N_REPS 50
+#define DEFAULT_SLOWDOWN 10000
+
+int main(int argc, char * argv[]) {
+    int i;
+    int slow_down = DEFAULT_SLOWDOWN;
+
+    if (argc == 1) {
+        fprintf(stderr, "Usage: %s string [delay]\n", argv[0]);
+        return 1;
+    }
+    if (argc >= 3) {
+        slow_down = atoi(argv[2]);
+    }
+
+    for (i = 0; i < N_REPS; i++) {
+        while (system("mkdir junk") != 0) {
+            char * cp = argv[1];
+            
+            while (*cp) {
+                printf("%c", *cp++);
+                fflush(stdout);
+                usleep(random() % slow_down);
+            }
+            system("rmdir junk"); 
+            usleep(5000);
+        }
+    }
+    return EXIT_SUCCESS;
+}
